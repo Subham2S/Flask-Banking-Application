1715cc83b3de73bee85e49cd5e25c4d188b6cf9f
Subham Shit <subham.besume@gmail.com>
2022-07-20 16:48:53 +0530

    Initial Commit 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return "<p>Hello, World! My Friend</p>"
+
+
+@app.route("/userinput")
+def userinput():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
